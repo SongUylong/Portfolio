85cfaf6b34ff1ad29cf4522f4758c400791f74d6
SongUylong <songuylong24@gmail.com>
2025-06-23 18:47:06 +0700

    Remove favicon.ico and update layout metadata to include new icon paths.

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,11 @@ const chewy = Chewy({
 export const metadata: Metadata = {
   title: "Uylong Song",
   description: "Portfolio",
+  icons: {
+    icon: "/logo.png",
+    shortcut: "/logo.png",
+    apple: "/logo.png",
+  },
 };
 
 export default function RootLayout({
